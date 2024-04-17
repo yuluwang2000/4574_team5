0ba5a21589b0d5f7e8e45ae7858ccf6e415e436a
Sitong Chen <sitongchenus@gmail.com>
2024-04-17 16:55:08 +0000

    solve the duplicate session_id and order_id

--- models/base/snowflake/base_snowflake__sessions.sql
+++ models/base/snowflake/base_snowflake__sessions.sql
@@ -1,10 +1,24 @@
 SELECT
     IP,
-    OS, 
-    CAST(CLIENT_ID AS STRING) AS CLIENT_ID,
-    SESSION_AT AS SESSION_AT_TS,
+    OS,
+    CLIENT_ID,
+    SESSION_AT_TS,
     SESSION_ID,
     _FIVETRAN_DELETED,
     _FIVETRAN_ID,
-    _FIVETRAN_SYNCED AS _FIVETRAN_SYNCED_TS
-FROM {{ source('snowflake','sessions')}}
+    _FIVETRAN_SYNCED_TS
+FROM (
+    SELECT
+        IP,
+        OS,
+        CAST(CLIENT_ID AS STRING) AS CLIENT_ID,
+        SESSION_AT AS SESSION_AT_TS,
+        SESSION_ID,
+        _FIVETRAN_DELETED,
+        _FIVETRAN_ID,
+        _FIVETRAN_SYNCED AS _FIVETRAN_SYNCED_TS,
+        ROW_NUMBER() OVER (PARTITION BY SESSION_ID ORDER BY SESSION_AT) AS rn
+    FROM
+        {{ source('snowflake','sessions')}}
+) AS sessions_with_rank
+WHERE rn = 1
