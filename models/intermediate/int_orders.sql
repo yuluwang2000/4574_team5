2c4faae6fd519acdaa42aff0637362da9eb9db26
Sitong Chen <sitongchenus@gmail.com>
2024-04-17 19:18:49 +0000

    aggregate items for each order_id; calculate the total price.

--- models/intermediate/int_orders.sql
+++ models/intermediate/int_orders.sql
@@ -1,29 +1,44 @@
 SELECT 
-    s.CLIENT_ID, 
-    iv.ITEM, 
-    iv.ADD_TO_CART_QUANTITY, 
-    iv.PRICE_PER_UNIT, 
-    o.SESSION_ID, 
-    o.ORDER_ID, 
+    o.ORDER_ID,
+    s.CLIENT_ID,
+    ARRAY_AGG(iv.ITEM) as ITEMS,
+    SUM(iv.ADD_TO_CART_QUANTITY - COALESCE(iv.REMOVE_FROM_CART_QUANTITY, 0)) as TOTAL_QUANTITY,
+    SUM((iv.ADD_TO_CART_QUANTITY - COALESCE(iv.REMOVE_FROM_CART_QUANTITY, 0)) * iv.PRICE_PER_UNIT) as TOTAL_PRICE,
+    o.SESSION_ID,
     CASE 
-        WHEN r.RETURNED_DATE IS NOT NULL THEN 1
+        WHEN MAX(r.RETURNED_DATE) IS NOT NULL THEN 1
         ELSE 0 
     END AS HAS_BEEN_RETURNED,
-    r.RETURNED_DATE,
-    r.IS_REFUNDED, 
+    MAX(CASE WHEN r.IS_REFUNDED IS NOT NULL THEN 1 ELSE 0 END) AS IS_REFUNDED,
+    o.CLIENT_NAME, 
     o.ISO_CODE, 
     o.SHIPPING_COST, 
-    o.ORDER_AT_TS,   
+    o.ORDER_AT_TS, 
+    o.PAYMENT_INFO, 
+    o.PAYMENT_METHOD, 
+    o.PHONE, 
     o.SHIPPING_ADDRESS, 
     o.STATE_NAME, 
     o.TAX_RATE
 FROM 
-    (SELECT *, ROW_NUMBER() OVER (PARTITION BY ORDER_ID ORDER BY ORDER_AT_TS) AS rn FROM {{ ref('base_snowflake__orders') }}) o
+    {{ ref('base_snowflake__orders') }} o
 LEFT JOIN 
     {{ ref('base_snowflake__sessions') }} s ON o.SESSION_ID = s.SESSION_ID
 INNER JOIN 
     {{ ref('base_snowflake__item_views') }} iv ON o.SESSION_ID = iv.SESSION_ID
 LEFT JOIN 
     {{ ref('base_google_drive__returns') }} r ON o.ORDER_ID = r.ORDER_ID
-WHERE
-    o.rn = 1
+GROUP BY 
+    s.CLIENT_ID,
+    o.SESSION_ID, 
+    o.ORDER_ID, 
+    o.CLIENT_NAME, 
+    o.ISO_CODE, 
+    o.SHIPPING_COST, 
+    o.ORDER_AT_TS, 
+    o.PAYMENT_INFO, 
+    o.PAYMENT_METHOD, 
+    o.PHONE, 
+    o.SHIPPING_ADDRESS, 
+    o.STATE_NAME, 
+    o.TAX_RATE
